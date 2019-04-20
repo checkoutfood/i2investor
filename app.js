7880881a46f316de28c1396552a1411916bd9fda
CHOCKALINGAM SUBRAMANIAN <checkoutfood.contact@gmail.com>
2019-04-20 21:21:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+require('./config/config');
+require('./models/db');
+require('./config/passportConfig');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose'); // Node Tool for MongoDB
+
+const rtsIndex = require('./routes/index.router');
+
+var app = express();
+
+mongoose.connect("mongodb+srv://checkoutfood:checkoutfood123@cluster0-5ffrd.mongodb.net/test?retryWrites=true", {
+  useMongoClient: true,
+}, (err) => {
+  // Check if database was able to connect
+  if (err) {
+    console.log('Could NOT connect to database: ', err); // Return error message
+  } else {
+    console.log('Connected to ' + config.db); // Return success message
+  }
+});
+
+
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(passport.initialize());
+app.use('/api', rtsIndex);
+
+// error handler
+app.use((err, req, res, next) => {
+    if (err.name === 'ValidationError') {
+        var valErrors = [];
+        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+        res.status(422).send(valErrors)
+    }
+    else{
+        console.log(err);
+    }
+});
+
+// start server
+app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
