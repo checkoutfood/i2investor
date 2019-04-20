9f63fcecebe1aeee9ea63f202a675f0b9eb2174d
CHOCKALINGAM SUBRAMANIAN <checkoutfood.contact@gmail.com>
2019-04-20 23:33:08 +0100

    initial commit

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ require('./models/db');
 require('./config/passportConfig');
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const passport = require('passport');
+const app = express();
 const mongoose = require('mongoose'); // Node Tool for MongoDB
-const env = require('./env');
+const config = require('./config/database'); // Mongoose Config
+const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+const port = process.env.PORT || 8080; // Allows heroku to set port
 
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const env = require('./env');
 const rtsIndex = require('./routes/index.router');
 
-var app = express();
-
-mongoose.connect("mongodb+srv://checkoutfood:checkoutfood123@cluster0-5ffrd.mongodb.net/test?retryWrites=true", {
+mongoose.connect('mongodb+srv://checkoutfood:checkoutfood123@cluster0-5ffrd.mongodb.net/test?retryWrites=true', {
   useMongoClient: true,
 }, (err) => {
   // Check if database was able to connect
@@ -27,25 +28,25 @@ mongoose.connect("mongodb+srv://checkoutfood:checkoutfood123@cluster0-5ffrd.mong
 
 
 // middleware
-app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:4200' })); // Allows cross origin in development only
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
 app.use(passport.initialize());
 app.use('/api', rtsIndex);
-app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
+
 
 // error handler
-app.use((err, req, res, next) => {
-    if (err.name === 'ValidationError') {
-        var valErrors = [];
-        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
-        res.status(422).send(valErrors)
-    }
-    else{
-        console.log(err);
-    }
-});
+// app.use((err, req, res, next) => {
+//     if (err.name === 'ValidationError') {
+//         var valErrors = [];
+//         Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+//         res.status(422).send(valErrors)
+//     }
+//     else{
+//         console.log(err);
+//     }
+// });
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
--- config/config.js
+++ config/config.js
@@ -1,17 +1,7 @@
-const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
 
-// Export config object
-module.exports = {
-//  uri: process.env.databaseUri, // Databse URI and database name
-  uri: "mongodb://checkoutfood:checkoutfood123@cluster0-5ffrd.mongodb.net/",
-  secret: crypto, // Cryto-created secret
-  db: 'test'
+var env = process.env.NODE_ENV || 'development';
 
-}
+var config = require('./config.json');
+var envConfig = config[env];
 
-// var env = process.env.NODE_ENV || 'development';
-
-// var config = require('./config.json');
-// var envConfig = config[env];
-
-// Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
