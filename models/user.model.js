be6865a918c3b492c5f9105c743c8e804b9c25e9
CHOCKALINGAM SUBRAMANIAN <checkoutfood.contact@gmail.com>
2019-04-21 06:16:06 +0100

    initial commit

--- models/user.model.js
+++ models/user.model.js
@@ -10,7 +10,7 @@ var userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: 'Email can\'t be empty',
-      //  unique: true
+        unique: true
     },
     password: {
         type: String,
